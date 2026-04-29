a8f153080286e521f9091dc2bf39a5d54be655ce
kumi-ikematsu <GitHub>
2026-04-29 13:11:55 +0900

    initial commit

---
+++ src/main/java/com/example/shokuji_kanri/ShokujiKanriApplication.java
@@ -0,0 +1,13 @@
+package com.example.shokuji_kanri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShokujiKanriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShokujiKanriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shokuji_kanri/ShokujiKanriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shokuji_kanri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShokujiKanriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
